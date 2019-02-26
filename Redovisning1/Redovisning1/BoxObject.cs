e65be21385c45f5e7c1f6fe9941ff3d966c5552b
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-02-26 16:40:15 +0100

    Försök att få rätt info från form
    Detta kommer vi sitta hela natten med :/

--- Redovisning1/Redovisning1/BoxObject.cs
+++ Redovisning1/Redovisning1/BoxObject.cs
@@ -10,8 +10,8 @@ namespace Redovisning1 {
     class BoxObject {
         Rectangle boxRect, boxSource;
         Texture2D spriteSheet;
-        Vector2 pos, startPos, pixelPos, friction, direction, speed, a;
-        float rotation, time, direction2, startSpeed, friktion, u, g;
+        Vector2 pos, startPos, pixelPos, friction, direction, speed, a, friktion;
+        float rotation, time, direction2, startSpeed, u, g;
         bool isRunning;
 
         public BoxObject(Texture2D spriteSheet, float rotation) {
@@ -23,10 +23,11 @@ namespace Redovisning1 {
             g = -9.82f;
             startSpeed = 0;
             time = 0;
-            u = 0;
+            u = 0.98f;
             a = new Vector2((float)(-g * Math.Cos(rotation)), (float)(g * Math.Sin(rotation)));
             //speed = new Vector2(0,-9.82f);
             speed = new Vector2((float)(startSpeed * Math.Cos(rotation)), (float)(startSpeed * Math.Sin(rotation)));
+            friktion = new Vector2(0, 0);
             boxRect = new Rectangle((int)pos.X, (int)pos.Y, 25, 25);
             boxSource = new Rectangle(0, 1, 25, 25);
             direction = new Vector2((float)Math.Cos(rotation), (float)Math.Sin(rotation));
@@ -40,6 +41,7 @@ namespace Redovisning1 {
                 Fall(gameTime);
             }
             else {
+                //u = Form1.Fri();
             }
             pixelPos = Conversion.PosToPixel(pos);
             boxRect.X = (int)pixelPos.X;
@@ -52,11 +54,18 @@ namespace Redovisning1 {
 
         public void Fall(GameTime gameTime) {
             time += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            friktion = (float) (g * Math.Sin((Math.PI / 180) * 60) * u); //tror man ska ha * time för att Normalkraften ändras men inte säker
-            //a.X -= friktion; //inte helt säker hur vi ska göra här
-            //a.Y -= friktion;
-            pos.X = (float)(startPos.X + ((a.X * Math.Pow(time, 2)) / 2));
-            pos.Y = (float)(startPos.Y + ((a.Y * Math.Pow(time, 2)) / 2));
+            friktion.X = (float) (-g * Math.Cos((Math.PI / 180) * 30) * u); //tror man ska ha * time för att Normalkraften ändras men inte säker
+            friktion.Y = (float)(g * Math.Sin((Math.PI / 180) * 30) * u);
+
+            //a.X -= friktion.X; //inte helt säker hur vi ska göra här
+            //a.Y -= friktion.Y;
+
+            //pos.X = (float)(startPos.X + (((a.X) * Math.Pow(time, 2)) / 2) - (((friktion.X) * Math.Pow(time, 2)) / 2));
+            //pos.Y = (float)(startPos.Y + (((a.Y) * Math.Pow(time, 2)) / 2) - (((friktion.Y) * Math.Pow(time, 2)) / 2));
+
+            pos.X = (float)(startPos.X + (((a.X) * Math.Pow(time, 2)) / 2) - friktion.X); //hur skulle hända om man släpper en låda för en backe... den skulle väll fortfarande öka hastigheten även fast det är friktion eller kommer den tillslut att stanna?
+            pos.Y = (float)(startPos.Y + (((a.Y) * Math.Pow(time, 2)) / 2) - friktion.Y);
+
             //pos += speed;
             //pos.X += speed.X * direction.X;
             //pos.Y += speed.Y * direction.Y;
--- Redovisning1/Redovisning1/Form1.cs
+++ Redovisning1/Redovisning1/Form1.cs
@@ -12,27 +12,44 @@ namespace Redovisning1 {
     public partial class Form1 : Form {
         BoxObject boxO;
         float friktion;
+        bool isPlaying, isAlive;
 
         public Form1() {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e) {
+            isPlaying = false;
+            isAlive = true;
+        }
+        //static public float Fri() {
+        //    float friktion = (float)NumericUpDown1.Value;
+        //    return friktion;
+        //}
 
-
+        public float GetFri {
+            get { return friktion; }
         }
 
+        public bool State {
+            get { return isPlaying; }
+        }
+
+        public bool AliveState {
+            get { return isAlive; }
+        }
 
         private void btn_Start_Click(object sender, EventArgs e) {
             friktion = (float)numericUpDown1.Value;
+            isPlaying = true;
             //numericUpDown1.Visible = false; //Ska ändras så att rutan blir grå tills bollen dör
             //Skickar värdet från textrutan till BoxObject, och ändrar isRunning till true
         }
 
         private void btn_Restart_Click(object sender, EventArgs e) {
-            //boxO.Kill(); //Kommer inte åt boxO klassen
-            
             numericUpDown1.Value = 0;
+            isAlive = false;
+            isPlaying = false;
             //Ändrar värdet som står i textrutan till 0, raderar alla boxar i boxList så att en ny skapas
             //Detta ska även hända om bollen åker utanför... ska vi ha så att man bara kan starta om genom att klicka på denna knapp tror det blir lättast så
         }
