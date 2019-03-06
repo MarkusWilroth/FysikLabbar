4f2b4b60d0bbc95bcbb12c6bf31a5a61013c1680
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-03-06 15:50:02 +0100

    Random C2

--- Redovisning2/Redovisning2/CarObject.cs
+++ Redovisning2/Redovisning2/CarObject.cs
@@ -9,9 +9,9 @@ using System.Threading.Tasks;
 namespace Redovisning2 {
     class CarObject {
         int speed;
-        float my, curveR, Fc, Fr, g, v, rotation, time, curve, angle, w, fMax, dir, vel;
+        float my, curveR, Fc, Fr, g, v, rotation, time, curve, angle, ac, fMax, dir, vel;
         Texture2D spriteSheet;
-        Vector2 carPos, startPos, convertedPos, friction, direction, velocity;
+        Vector2 carPos, startPos, convertedPos, aDirection, direction, velocity, centripetalDirection;
         Rectangle carRect, carSource, roadSource;
         Form1 form;
 
@@ -21,26 +21,29 @@ namespace Redovisning2 {
             this.rotation = rotation;
             
             g = 9.82f;
-            speed = form.GetSpeed();
-            v = (float)(speed / 3.6);
-            my = form.GetMy();
-            curveR = form.GetCurve();
-            curve = form.SendCurve();
+            speed = form.GetSpeed(); //Får speed i km/h
+            v = (float)(speed / 3.6); //Ändrar det till m/s
+            my = form.GetMy(); //Får my
+            curveR = form.GetCurve(); //Får radien av cirkeln
             Fr = my * g;
+            Fc = (float)(Math.Pow(v, 2) / curveR);
+            if (Fr > Fc) {
+                Console.WriteLine("Success");
+            }
+
+            aDirection = new Vector2(0, 0);
+            ac = (float)Math.Pow(v,2) / curveR;
+            fMax = g * my;
+
             angle = 0;
             time = 0;
-            Fc = (float)(Math.Pow(v, 2) / curveR);
-            
             carPos = new Vector2(curveR, 0);
+            velocity = new Vector2(0, 0);
             startPos = carPos;
-            fMax = (float)(g * (Math.Sin(rotation) - (Math.Cos(rotation) * my)));
             carRect = new Rectangle((int)carPos.X, (int)carPos.Y, 15, 25);
             carSource = new Rectangle(5, 5, 15, 25);
             roadSource = new Rectangle(0, 31, 514, 716);
-            if (Fr > Fc) {
-                Console.WriteLine("Success?");
-            }
-            w = v / curveR;
+            
         }
         public void Update(GameTime gameTime) {
             Drive(gameTime);
@@ -49,30 +52,35 @@ namespace Redovisning2 {
         }
 
         public void Drive(GameTime gameTime) {
-            time += (float)gameTime.ElapsedGameTime.TotalSeconds;
-
             //angle = (float)(w * time);
             //carPos.X = (float)Math.Cos(angle) * v; //Startar inte där den ska starta och har inte friktion med i uträkningen?!?!
             //carPos.Y = (float)(Math.Sin(angle) * v);
 
             //Fr = (float)(g * (Math.Sin(angle) - (Math.Cos(angle) * my)));
-            direction = new Vector2((float)(Math.Cos(angle)*Math.PI/180), (float)(Math.Sin(angle)*Math.PI/180)*v);
-            velocity = direction + friction;
-            angle = (float)(Math.Atan2(velocity.Y, velocity.X) * 180/Math.PI);
-            Fc = (float)((Math.Pow(v, 2) / curveR));
-            friction.X = (float)(Fc * Math.Cos(angle));
-            friction.Y = (float)(Fc * Math.Sin(angle));
-            
+            time = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            angle = (float)(Math.Atan2(velocity.X, velocity.Y) * 180 / Math.PI);
 
-            carPos.X += (float)(velocity.X*time);
-                carPos.Y += (float)(velocity.Y*time);
+            //aDirection = new Vector2((float)Math.Cos(angle + 90), (float)(Math.Sin(angle + 90) * ac * (Math.Pow(time,2)/2)));
+            //direction = new Vector2((float)(Math.Cos(angle)*Math.PI/180), (float)((Math.Sin(angle)*Math.PI/180) * v * (Math.Pow(time, 2) / 2)));
 
+            //velocity = direction + aDirection;
+            //angle = (float)(Math.Atan2(velocity.Y, velocity.X) * 180 / Math.PI);
 
+            //carPos.X += (float)(velocity.X);
+            //carPos.Y += (float)(velocity.Y);
 
+            centripetalDirection.X = (float)(Fc * Math.Cos(angle) * time);
+            centripetalDirection.Y = (float)(Fc * Math.Sin(angle) * time);
 
-                //carPos.X = (float)(startPos.X + (((Fr) * Math.Pow(time, 2)) / 2));
-                //carPos.Y = (float)(startPos.Y + (((Fr) * Math.Pow(time, 2)) / 2));
-            
+            direction = new Vector2((float)(Math.Cos(angle) * Math.PI / 180), (float)((Math.Sin(angle) * Math.PI / 180) * v));
+            velocity = direction + centripetalDirection;            
+
+            carPos.X += (float)(velocity.X * time);
+            carPos.Y += (float)(velocity.Y * time);
+
+            //carPos.X = (float)(startPos.X + (((Fr) * Math.Pow(time, 2)) / 2));
+            //carPos.Y = (float)(startPos.Y + (((Fr) * Math.Pow(time, 2)) / 2));
+
         }
 
         public void Draw(SpriteBatch sb) {
